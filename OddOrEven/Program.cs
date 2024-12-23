709f1d0bd415b3f54cebac0998b1e49214c4d842
PizzaBot <Breiton.Peerna@gmail.com>
2024-12-23 12:19:02 +0200

    MYNEWPROJECT

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number!");
+
+int usernum = Convert.ToInt32(Console.ReadLine());
+
+// kas kasutaja arv on paaris voi paaritu
+
+int result = usernum % 2;
+if (result != 0) // ei ole vordne
+{
+    Console.WriteLine("user number is odd");
+} else
+{
+    Console.WriteLine("User number is even");
+}
